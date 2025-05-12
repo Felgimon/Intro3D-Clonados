cbc7f4bf605941f79dfe8858bebb3aa3f74d9722
Felgi <felipeferraridoval@gmail.com>
2025-05-12 12:13:29 -0300

    Initial commit

---
+++ Assets/Scripts/Instatiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+         
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cubePrefab);
+    }
+}
---
+++ Assets/Scripts/RandomCloner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+    public GameObject cylinderPrefab;
+    int min = 0;
+    int max = 2;
+    int randomNumber;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void Cloner()
+    {
+        randomNumber = Random.Range(min, max + 1);
+        if (randomNumber == 0)
+        {
+            Instantiate(cubePrefab);
+        }
+        else if (randomNumber == 1)
+        {
+            Instantiate(spherePrefab);
+        }
+        else if (randomNumber == 2)
+        {
+            Instantiate(cylinderPrefab);
+        }
+    }
+}
